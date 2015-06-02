661fe627c1b05b492a19b701f17c591fed0eb6ca
furukawa yutaka <frkw@kogasw.com>
2015-06-02 10:54:11 +0900

    error handling

--- src/main.rs
+++ src/main.rs
@@ -1,30 +1,33 @@
 use std::net::{TcpListener};
 use std::thread;
 use std::io::{Read, Write};
+use std::io;
 
-fn main() {
-    if let Ok(lis) = TcpListener::bind("0.0.0.0:12345") {
-        println!("started on {:?}", lis);
-        for stream in lis.incoming() {
-            if let Ok(mut stream) = stream {
-                println!("{:?} connected.", stream.peer_addr());
-                thread::spawn(move || {
-                    loop {
-                        let mut b = [0; 1024];
-                        if let Ok(n) = stream.read(&mut b) {
-                            if n == 0 {
-                                println!("{:?} closed.", stream.peer_addr());
-                                return;
-                            } else {
-                                let _ = stream.write(&b[0..n]);
-                            }
-                        } else {
-                            return;
-                        }
-                    }
-                });
+fn start() -> io::Result<()> {
+    let lis = try!(TcpListener::bind("0.0.0.0:12345"));
+    println!("started on {:?}", lis);
+    for stream in lis.incoming() {
+        let mut stream = try!(stream);
+        println!("{:?} connected.", try!(stream.peer_addr()));
+        let _: thread::JoinHandle<io::Result<()>> = thread::spawn(move || {
+            loop {
+                let mut b = [0; 1024];
+                let n = try!(stream.read(&mut b));
+                if n == 0 {
+                    println!("{:?} closed.", try!(stream.peer_addr()));
+                    return Ok(());
+                } else {
+                    try!(stream.write(&b[0..n]));
+                }
             }
-        }
+        });
     }
+    Ok(())
 }
 
+fn main() {
+    match start() {
+        Ok(_) => return,
+        Err(e) =>println!("{:?}", e)
+    }
+}
